90aea1bae31496d28e9717a44c6dd2a80f0eaa22
Keshav-C0des <keshavmandal2503@gmail.com>
2025-01-10 22:58:18 +0530

    Printing of binomial expansion of a term using Pascal Triangle

--- FY BE/Pascal _Triangle.cpp
+++ FY BE/Pascal _Triangle.cpp
@@ -1,72 +1,81 @@
 #include <iostream>
-#include<string>
-#include <io.h>
-#include <fcntl.h> // For _O_U16TEXT
+#include <string>
 using namespace std;
 
-    void binomial_expansion(int n)
+void binomial_expansion(int n)
+{
+    int length = 0;
+    for (int i = 1; i <= n + 1; i++)
     {
-        int length = 0;
-        for (int i = 1; i <= n + 1; i++)
+        for (int j = 1; j <= i; j++)
         {
-            for (int j = 1; j <= i; j++)
-            {
-                length = length + 1;
-            }
+            length = length + 1;
         }
-        cout << "Total number of coefficients in the triangle is " << length << endl
-             << endl;
+    }
+    cout << "Total number of coefficients in the triangle is " << length << endl
+         << endl;
 
-         int triangle[n + 1][length];
+    int triangle[n + 1][length];
 
-        for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n + i; j++)
         {
-            for (int j = 0; j <= n + i; j++)
+            if (j <= n - 1 - i)
             {
-                if (j <= n - 1 - i)
+                cout << "  ";
+            }
+            else if (n % 2 != 0 && ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) || (n % 2 == 0 && ((i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0))))
+            {
+                cout << "  ";
+            }
+            else if ((j == n - i) || (j == n + i))
+            {
+                triangle[i][j] = 1;
+                cout << triangle[i][j] << " ";
+            }
+            else
+            {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j + 1];
+                if (triangle[i][j] > 9)
                 {
-                    cout << "  ";
-                }
-                else if (n % 2 != 0 && ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) || (n % 2 == 0 && ((i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0))))
-                {
-                    cout << "  ";
-                }
-                else if ((j == n - i) || (j == n + i))
-                {
-                    triangle[i][j] = 1;
-                    cout << triangle[i][j] << " ";
+                    cout << triangle[i][j];
                 }
                 else
                 {
-                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j + 1];
-                    if (triangle[i][j] > 9)
-                    {
-                        cout << triangle[i][j];
-                    }
-                    else
-                    {
-                        cout << triangle[i][j] << " ";
-                    }
+                    cout << triangle[i][j] << " ";
                 }
             }
-            cout << endl;
         }
-
-        // Set the console output mode to handle UTF-16
-        _setmode(_fileno(stdout), _O_U16TEXT);
-
-        wchar_t power[10] = {L'⁰', L'¹', L'²', L'³', L'⁴', L'⁵', L'⁶', L'⁷', L'⁸', L'⁹'};
-        char var[2]={'a', 'b'};
-        //wcout << L"Superscript digits: " << power << endl;
-        int coeff[n], endi=n+1, endj=length;
-        for (int i = n; i >= 0; i--)
-        {
-            coeff[i] = triangle[endi][endj];
-            endj--;
-        }
-        wcout<<endl<<L"("<<var[0]<<L"+"<<var[1]<<L")"<<power[n]<<L" = ";
+        cout << endl;
     }
-    
+
+    int coeff[(2 * n) + 1];
+    int ap = 0, bp = n;
+    cout << endl
+         << "(a+b)^" << n << " = ";
+    for (int j = 2 * n; j >= 0; j--)
+    {
+        if (j % 2 != 0)
+        {
+            continue;
+        }
+        else
+        {
+            coeff[j] = triangle[n][j];
+        }
+        if (j == 0)
+        {
+            cout << coeff[j] << "(a)^(" << bp << ")." << "(b)^(" << ap << ")";
+        }
+        else
+        {
+            cout << coeff[j] << "(a)^(" << bp << ")." << "(b)^(" << ap << ")  +  ";
+        }
+        ap++;
+        bp--;
+    }
+}
 
 int main()
 {
