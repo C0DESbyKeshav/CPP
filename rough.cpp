9ef4e4fbb45c307d8248e9864a6762fb4e50cf94
C0DESbyKeshav <keshavmandal2503@gmail.com>
2025-04-24 12:01:08 +0530

    CPP

---
+++ rough.cpp
@@ -0,0 +1,15 @@
+/* Write a program to print this pattern by taking input how many rows to print.
+1
+11
+21
+1211
+111221
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
